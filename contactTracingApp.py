e23f6598092c283f66f2635605c6d6d4839153ec
StanleyPangaruy <pangaruystanley@gmail.com>
2022-02-17 17:13:06 +0800

    Current build allows the detection of a single qr code then the txt file output will be made.

--- contactTracingApp.py
+++ contactTracingApp.py
@@ -1,9 +1,32 @@
+#Contact Tracing App
+
+from distutils.text_file import TextFile
 import cv2
 from pyzbar.pyzbar import decode
+from datetime import datetime
+import os
 
-img = cv2.imread('testqrcode.png')
+#img = cv2.imread('qwert.png')
+cap = cv2.VideoCapture(2)
+cap.set(3,640)
+cap.set(4,480)
+now = datetime.now()
 
-for qrcode in decode(img):
-    myData = qrcode.data.decode('utf-8')
-    txtFile = open("testqrcodedata.txt", "x")
-    txtFile.write(myData)
+while True:
+    success, img = cap.read()
+    n = 0
+    for qrcode in decode(img):
+        dt = now.strftime("%m/%d/%Y %I:%M:%S %p PHT")
+        myData = qrcode.data.decode()
+        txtFile = open("qrCodeData.txt", "a")
+        txtFile.write(myData)
+        txtFile.write(f"\nLast scanned on {dt}.\n\n")
+        n =+ 1
+    cv2.imshow('Result', img)
+    cv2.waitKey(1)
+    if n ==  1:
+        break
+cap.release()
+cv2.destroyAllWindows
+
+
